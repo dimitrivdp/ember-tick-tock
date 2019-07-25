5571663b682a78b6d88d9915db665aab8e1c9c5b
Dimitri Van de Putte <dimitri@vandeputte.be>
2019-07-25 18:41:25 +0200

    bump to 2.18

--- addon/services/ticktock.js
+++ addon/services/ticktock.js
@@ -1,8 +1,11 @@
-import Ember from 'ember';
+import $ from 'jquery';
+import { later } from '@ember/runloop';
+import { set, get } from '@ember/object';
+import Service from '@ember/service';
+import { getOwner } from '@ember/application';
 import moment from 'moment';
-const { getOwner } = Ember;
 
-export default Ember.Service.extend({
+export default Service.extend({
   now: null,
   currentOffset: 0,
   useRemoteTimestamp: false,
@@ -12,6 +15,7 @@ export default Ember.Service.extend({
   timestampProperty: null,
 
   init() {
+    this._super(...arguments);
     this._loadConfigAndStartTimers();
   },
 
@@ -19,43 +23,43 @@ export default Ember.Service.extend({
     let config = getOwner(this).resolveRegistration('config:environment')['ticktockOptions'];
 
     if (config && config.remoteSyncFrequency) {
-      Ember.set(this, 'remoteSyncFrequency', config.remoteSyncFrequency);
+      set(this, 'remoteSyncFrequency', config.remoteSyncFrequency);
     }
 
     if (config && config.tickTockFrequency) {
-      Ember.set(this, 'tickTockFrequency', config.tickTockFrequency);
+      set(this, 'tickTockFrequency', config.tickTockFrequency);
     }
 
     if (config && config.timestampEndpoint) {
-      Ember.set(this, 'timestampEndpoint', config.timestampEndpoint);
+      set(this, 'timestampEndpoint', config.timestampEndpoint);
     }
 
     if (config && config.timestampProperty) {
-      Ember.set(this, 'timestampProperty', config.timestampProperty);
+      set(this, 'timestampProperty', config.timestampProperty);
     }
 
     if (config && config.useRemoteTimestamp) {
-      Ember.set(this, 'useRemoteTimestamp', config.useRemoteTimestamp);
+      set(this, 'useRemoteTimestamp', config.useRemoteTimestamp);
       this._setServerTime();
     }
 
     this._setCurrentTime();
   },
-  
+
   _syncServerLoop() {
-    let frequency = Ember.get(this, 'remoteSyncFrequency');
-    Ember.run.later(this, this._setServerTime, (frequency * 1000));
+    let frequency = get(this, 'remoteSyncFrequency');
+    later(this, this._setServerTime, (frequency * 1000));
   },
-  
+
   _syncLocalLoop() {
-    let frequency = Ember.get(this, 'tickTockFrequency');
-    Ember.run.later(this, this._setCurrentTime, (frequency * 1000));
+    let frequency = get(this, 'tickTockFrequency');
+    later(this, this._setCurrentTime, (frequency * 1000));
   },
-  
+
   _setServerTime() {
     let _this = this;
 
-    Ember.$.ajax(_this.timestampEndpoint, {
+    $.ajax(_this.timestampEndpoint, {
       type: 'GET',
       success: (data) => {
         var currentServerTime = data[_this.timestampProperty];
@@ -63,22 +67,22 @@ export default Ember.Service.extend({
         var serverTime        = parseInt(currentServerTime);
         var serverOffset      = serverTime - parseInt(currentLocalTime);
 
-        Ember.set(_this, 'currentOffset', serverOffset);
+        set(_this, 'currentOffset', serverOffset);
       }
     });
 
     this._syncServerLoop();
   },
-  
+
   _setCurrentTime: function() {
     let now = moment().unix();
-    
-    if (Ember.get(this, 'useRemoteTimestamp')) {
-      now += Ember.get(this, 'currentOffset');
+
+    if (get(this, 'useRemoteTimestamp')) {
+      now += get(this, 'currentOffset');
     }
 
     now = moment.unix(now);
-    Ember.set(this, 'now', now);
+    set(this, 'now', now);
 
     this._syncLocalLoop();
   }
--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -1,18 +1,20 @@
-import Ember from 'ember';
+import { computed } from '@ember/object';
+import { inject as service } from '@ember/service';
+import Controller from '@ember/controller';
 import moment from 'moment';
 
-export default Ember.Controller.extend({
-  ticktock: Ember.inject.service(),
+export default Controller.extend({
+  ticktock: service(),
 
-  timeFreeze: Ember.computed(function() {
+  timeFreeze: computed(function() {
     return moment();
   }),
 
-  formattedTime: Ember.computed('ticktock.now', function() {
+  formattedTime: computed('ticktock.now', function() {
     return this.get('ticktock.now').format('MM/DD/YYYY [at] hh:mm:ss');
   }),
 
-  formattedTimeFreeze: Ember.computed('timeFreeze', function() {
+  formattedTimeFreeze: computed('timeFreeze', function() {
     return this.get('timeFreeze').format('MM/DD/YYYY [at] hh:mm:ss');
   })
 });
